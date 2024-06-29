148f6e0b8289013e241d5692c800ca9299dcd654
Marcel James <marcelaribal083002@gmail.com>
2024-06-29 14:42:23 +0800

    feat: split dev & prod env

---
+++ get_random_secret_key.py
@@ -0,0 +1,8 @@
+from django.core.management.utils import get_random_secret_key
+
+print(get_random_secret_key())
+
+"""
+ This import get random secret key for your django settings
+"""
+# python get_random_secret_key.py
--- manage.py
+++ manage.py
@@ -2,11 +2,14 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
-
+from ez2erp.settings import base
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ez2erp.settings')
+    if base.DEBUG:
+        os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ez2erp.settings.local')
+    else:
+        os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ez2erp.settings.production')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
